a9f6f77e1e7029e0c97e0d2013be56712733d919
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-07-31 08:26:02 -0500

    Fixed attack

--- commands/attack.js
+++ commands/attack.js
@@ -143,7 +143,7 @@ module.exports.execute = async (client, message, args) => {
                               { where: { user: message.author.id } }
                             ).then(() => {
                               let plural = "coins";
-                              if (requestedcoins - penalty === 1) plural = "coins";
+                              if (result[0].coins - penalty === 1) plural = "coins";
                               var stole = `${plural}.`;
                               if (penalty == 0) stole = `, losing no ${plural} because of your thief class!`;
                               return message.channel.send(`You attacked! Stealing ${givenPoints} points from team ${wordtarget}. Their points are now at ${newPoints}, and yours are at ${teamresult[0].points + givenPoints}. You now have ${result[0].coins - penalty} ${stole}`);}).catch((err) => {
