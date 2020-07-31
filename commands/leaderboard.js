42bb48ef0d46969b7b581cda730dd7057b113a44
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-07-31 08:32:18 -0500

    Removed human leaderboard

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,104 +1,49 @@
 const Discord = require('discord.js');
-const pomMembers = require('../databaseFiles/pomMembers');
 const pomTeams = require('../databaseFiles/pomTeams');
 const Sequelize = require('sequelize');
 
-module.exports.execute = async (client, message, args) => {
-  var focus;
-
-  if (args[0]) {
-    if (args[0].toLowerCase() == "teams") {
-      focus = "teams";
-    } else {
-      focus = "humans";
-    }
-  } else {
-    focus = "humans";
-  }
+module.exports.execute = async (client, message) => {
 
   try {
-    if (focus == "humans") {
-      pomMembers.sync().then(() => {
-        pomMembers.findAll({
-          attributes: [
-              "id", "user", "points", "team",
-              [Sequelize.literal('RANK () OVER ( ORDER BY points DESC )'), 'rank']
-            ],
-            raw: true,
-        }).then((result) => {
-            if (result.length >= 1) {
-              const guild = client.guilds.cache.get(message.guild.id);
+    pomTeams.sync().then(() => {
+      pomTeams.findAll({
+        attributes: [
+            "id", "team", "points",
+            [Sequelize.literal('RANK () OVER ( ORDER BY points DESC )'), 'rank']
+          ],
+          raw: true,
+      }).then((result) => {
+          if (result.length >= 1) {
 
-              let helpMessage = new Discord.MessageEmbed()
-                .setColor('#750384')
-                .setTitle('The Leaderboard')
-                .setDescription(
-                  `See the top 10 users in the fight!`
+            let helpMessage = new Discord.MessageEmbed()
+              .setColor('#750384')
+              .setTitle('Team Leaderboard')
+              .setDescription(
+                `See the ranks of teams!`
+              );
+
+              for(var i = 0; i < result.length; ++i){
+                var prettynumber = i + 1;
+
+                helpMessage.addField(
+                  `**${prettynumber}**`,
+                  `\`\`\`${result[i].team[0].toUpperCase() + result[i].team.slice(1)} - ${result[i].points}\`\`\``
                 );
 
-                for(var i = 0; i < result.length; ++i){
-                  var member = guild.members.cache.get(result[i].user);
-
-                  var prettynumber = i + 1;
-
-                  helpMessage.addField(
-                    `**${prettynumber}**`,
-                    `\`\`\`${member.user.username} - ${result[i].points} (Team ${result[i].team})\`\`\``
-                  );
-
-                  if (i == 9) break;
-                }
-              try {
-                return message.channel.send(helpMessage);
-              } catch (err) {
-                console.log(err);
+                if (i == 9) break;
               }
-            } else {
-              return message.channel.send('I couldn\'t find that user! Please try again.');
+            try {
+              return message.channel.send(helpMessage);
+            } catch (err) {
+              console.log(err);
             }
-        });
+          } else {
+            return message.channel.send('I couldn\'t find that user! Please try again.');
+          }
       });
-    } else {
-      pomTeams.sync().then(() => {
-        pomTeams.findAll({
-          attributes: [
-              "id", "team", "points",
-              [Sequelize.literal('RANK () OVER ( ORDER BY points DESC )'), 'rank']
-            ],
-            raw: true,
-        }).then((result) => {
-            if (result.length >= 1) {
-
-              let helpMessage = new Discord.MessageEmbed()
-                .setColor('#750384')
-                .setTitle('Team Leaderboard')
-                .setDescription(
-                  `See the ranks of teams!`
-                );
-
-                for(var i = 0; i < result.length; ++i){
-                  var prettynumber = i + 1;
-
-                  helpMessage.addField(
-                    `**${prettynumber}**`,
-                    `\`\`\`${result[i].team[0].toUpperCase() + result[i].team.slice(1)} - ${result[i].points}\`\`\``
-                  );
-
-                  if (i == 9) break;
-                }
-              try {
-                return message.channel.send(helpMessage);
-              } catch (err) {
-                console.log(err);
-              }
-            } else {
-              return message.channel.send('I couldn\'t find that user! Please try again.');
-            }
-        });
-      });
-    }
+    });
   } catch (err) {
-    console.error("Sequelize error: ", err);
+    console.log(err);
   }
 };
 
