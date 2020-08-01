39e435ec6e33afcb5038731e09c8c399530c843a
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-08-01 15:46:23 -0500

    Update drop command (again)

--- commands/drop.js
+++ commands/drop.js
@@ -19,7 +19,7 @@ module.exports.execute = async (client, message) => {
           pomTeams.sync().then(() => {
             pomMembers.sync().then(() => {
               pomTeams.findAll({
-                attributes: [knights, thieves, jokers, stonemasons]
+                attributes: ['knights', 'thieves', 'jokers', 'stonemasons']
               }).then((result) => {
                 var members = result[0].knights + result[0].thieves + result[0].jokers + result[0].stonemasons + result[1].knights + result[1].thieves + result[1].jokers + result[1].stonemasons + result[2].knights + result[2].thieves + result[2].jokers + result[2].stonemasons;
 
