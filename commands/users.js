36cf36784f348ff42d294b11dadd00937491c47c
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-09-27 10:44:49 -0500

    Testing

--- commands/users.js
+++ commands/users.js
@@ -91,6 +91,10 @@ module.exports.execute = async (client, message, args) => {
         var lines = 10;
         if (result.length < 10) lines = result.length;
 
+        console.log(page);
+        console.log(page * 10);
+        console.log(result[(page * 10)]);
+
         if (!result[(page * 10)]) return message.channel.send(':x: Looks like your page number is out of range! Try again with a lower page number.');
 
         for (var i = page * 10; i < (page * 10) + lines; ++i) {
