bce838f34c3808cce9b06f4f21a517b6ac3eea01
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-09-27 10:57:17 -0500

    Testing

--- commands/users.js
+++ commands/users.js
@@ -99,9 +99,11 @@ module.exports.execute = async (client, message, args) => {
 
         if (!result[(page * 10)]) return message.channel.send(':x: Looks like your page number is out of range! Try again with a lower page number.');
         if (((page*10)-1)-result.length > 0) {
-          lines = lines - (page-1)*10;
+          lines = lines - (page-1)*10 - 1;
         }
-        
+
+        console.log(lines);
+
         for (var i = page * 10; i < (page * 10) + lines; ++i) {
           if (result[i].user) {
             var member = guild.members.cache.get(result[i].user);
